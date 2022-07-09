7809ac7c9c46ae90434a32a313a4c0fe0591e406
iradakhf <iradakhf@code.edu.az>
2022-07-09 21:23:20 +0400

    add all

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace ConsoleApp17
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            string word = "pro";
+            int i = 0;
+
+            ProDocumentProgram pro = new ProDocumentProgram();//pro sozune uygun muvafiq obyekt pro
+            ExpertDocumentProgram expert = new ExpertDocumentProgram();
+            DocumentProgram basic = new DocumentProgram();
+            
+            
+           
+                if (word=="pro")
+                {
+                    pro.OpenDocument();
+                    pro.EditDocument();
+                    pro.SaveDocument();
+                    
+                }
+                else if (word=="basic")
+                {
+                    basic.OpenDocument();
+                    basic.EditDocument();
+                    basic.SaveDocument();
+                    
+                }
+                else
+                {
+                    basic.OpenDocument();
+                    basic.EditDocument();
+                    basic.SaveDocument();
+                    
+                }
+             
+
+
+
+        }
+    }
+}
